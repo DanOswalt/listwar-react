4fba834f82e9e07d81c1a6acc7b3fb81a8adcf55
Dan Oswalt <do189705@gmail.com>
2019-04-06 20:58:06 -0400

    result with basic tiebreaks

--- src/components/pages/List/ListWar.js
+++ src/components/pages/List/ListWar.js
@@ -10,19 +10,6 @@ class ListWar extends Component {
   constructor(props) {
     super(props);
 
-    // const mockList = {
-    //   title: "Mock List",
-    //   entries: [
-    //     "Pizza",
-    //     "Bananas",
-    //     "Fish",
-    //     "Lasagna",
-    //     "Grapes",
-    //     "Orange",
-    //     "Cheese"
-    //   ]
-    // }
-
     this.state = {
       Chance: new chance(),
       pageTitle: "War!",
@@ -74,6 +61,7 @@ class ListWar extends Component {
       items: currentList.entries.map((value, index) => {
         return {
           value: value,
+          wins: 0,
           points: 0,
           id: index,
           beats: [],
@@ -113,7 +101,6 @@ class ListWar extends Component {
     matchIndex++;
 
     if (matchIndex === schedule.length) {
-      // finish method
       console.log('result:', this.state.currentResult);
       this.setState({ currentMatch: {
         hero: { value: "", listIndex: -1 },
@@ -135,9 +122,11 @@ class ListWar extends Component {
 
   pickWinner = (winnerIndex, loserIndex) => {
     const { currentResult } = this.state;
-    currentResult.items[winnerIndex].points += 1;
-    currentResult.items[winnerIndex].beats.push(loserIndex);
-    this.nextMatch()
+    const winner = currentResult.items[winnerIndex];
+    winner.wins += 1;
+    winner.points += 1;
+    winner.beats.push(loserIndex);
+    this.nextMatch();
   }
 
   finish = () => {
@@ -146,7 +135,85 @@ class ListWar extends Component {
   }
 
   processResult() {
-    // do something
+    const result = JSON.parse(JSON.stringify(this.state.currentResult));
+    const pointsTiers = {};
+
+    const _descByPoints = (a, b) => b.points - a.points;
+
+    const _setItemRank = (item, index) => item.rank = index + 1;
+
+    const _createPointsTiers = item => {
+      const pts = item.points;
+      if (!pointsTiers[pts]) { // if doesn't exist, create the tier
+        pointsTiers[pts] = []
+      }
+      pointsTiers[pts].push(item.id);
+    }
+
+    result.items.forEach(_createPointsTiers);
+    this.doHeadToHeadTiebreaks(pointsTiers, result);
+    console.log(result)
+    console.log(this.state.currentResult)
+
+    // result.items.forEach(_createPointsTiers);
+    // this.doBestBeatComparisonTiebreak(pointsTiers, result);
+
+    result.items.forEach(_createPointsTiers);
+    this.doRandomTiebreak(pointsTiers, result);
+
+    result.items.sort(_descByPoints);
+    result.items.forEach(_setItemRank);
+
+    this.setState({ currentResult: result});
+  }
+
+  doHeadToHeadTiebreaks(pointsTiers, result) {
+    for (const tier in pointsTiers) {
+      const tiedPlayers = pointsTiers[tier];
+      if (tiedPlayers.length === 1) return;
+
+      tiedPlayers.forEach(heroId => {
+        const hero = result.items[parseInt(heroId)];
+        tiedPlayers.forEach(villainId => {
+          if (hero.beats.includes(villainId)) {
+            hero.points += 0.1;
+          }
+        })
+      })
+    }
+    // console.log("h2h tiebreaks", result);
+  }
+
+  // doBestBeatComparisonTiebreak(pointsTiers, result) {
+  //   for (const tier in pointsTiers) {
+  //     const tiedPlayers = pointsTiers[tier];
+  //     const comparatorIndex = 0;
+
+  //     if (tiedPlayers.length === 1) return;
+
+  //     tiedPlayers.forEach(heroId => {
+  //       const hero = result.items[parseInt(heroId)];
+  //       hero.beats.sort((a, b) => {
+  //         const beatA = result.items[parseInt(a)];
+  //         const beatB = result.items[parseInt(b)];
+
+  //         return beatB.points - beatA.points;
+  //       });
+  //     });
+  //   }
+  //   // console.log("bb tiebreaks", result);
+  // }
+
+  doRandomTiebreak(pointsTiers, result) {
+    for (const tier in pointsTiers) {
+      const tiedPlayers = pointsTiers[tier];
+      if (tiedPlayers.length === 1) return;
+      tiedPlayers.forEach(heroId => {
+        const hero = result.items[parseInt(heroId)];
+        hero.points += Math.random() * .001;
+      })
+    }
+    // console.log("rando tiebreaks", result)
   }
 
   render () {
