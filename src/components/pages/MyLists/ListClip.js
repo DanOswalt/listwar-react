9846895a7c8ee558dbf74053e8e4acb32fa10e60
Dan Oswalt <do189705@gmail.com>
2019-05-05 09:18:28 -0400

    remove alias from mylist clips

--- src/components/pages/MyLists/ListClip.js
+++ src/components/pages/MyLists/ListClip.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const ListClip = ({ list, userAlias }) => {
-  const { title, url, winner, resultId, alias } = list;
+const ListClip = ({ list }) => {
+  const { title, url, winner, resultId } = list;
   const icon = resultId && <i className="nes-icon trophy is-small"></i>;
   const item = resultId ? <div className="winner-entry">{winner}</div> : "--not yet completed--";
 
   return (
     <div className="list-clip-container">
       <Link to={url}>
-        <div className="little-alias-box">
-          <i className="nes-icon is-small star"></i>
-            <span className="nes-text little-alias">{alias}</span>
-          <i className="nes-icon is-small star"></i>
-        </div>
         <div className="list-clip nes-container with-title is-dark is-rounded">
           <p className="title">{title}</p>
           {icon}{item}
